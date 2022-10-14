cc9f6c83204a79f248c06cd1961eeb089c76c8c2
JustSomeNoname <noname.discord@protonmail.com>
2022-10-14 23:53:42 +0200

    first commit

---
+++ assets/config.js
@@ -0,0 +1,23 @@
+const config = {
+    
+    USERNAME: "Noname / ノナメ#5221",
+    PICTURE: "assets/img/profile_picture.gif",
+
+    DESCRIPTION: '<i>he/him</i><br><br><b>Staff @ <a href="https://discord.com/invite/amar">Amar</a>, <a href="https://discord.com/invite/rocketment"> Rocketment</a> & <a href="https://discord.com/invite/e-bio-913922450205585439">ebio.gg</a></b><br><br><b> Anime & Gaming Cafe </b><br><b>ᗇ <u><a href="https://discord.com/invite/animegamingcafe">https://discord.gg/animegamingcafe</u></b>',
+
+    BADGES: [
+        { id: "HypeSquad Balance", image: "assets/img/hypesquad_balance.png" },
+        { id: "Nitro Sub", image: "assets/img/nitro_sub.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/amar.gif", link: "https://discord.com/invite/amar", name: "Amar" },
+        { id: "project-2", image: "assets/img/rocketment.gif", link: "https://discord.com/invite/rocketment", name: "Rocketment" },
+        { id: "project-3", image: "assets/img/ebio.gif", link: "https://discord.com/invite/e-bio-913922450205585439", name: "e.bio" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/JustSomeNoname" },
+    ],
+
+}
