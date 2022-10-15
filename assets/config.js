5cfc5be7d3ea3c36d855783836ad0d9f67083841
JustSomeNoname <noname.discord@protonmail.com>
2022-10-15 12:38:21 +0200

    updated some things

--- assets/config.js
+++ assets/config.js
@@ -1,25 +1,55 @@
 const config = {
-    
+
     USERNAME: "Noname / ノナメ#5221",
     PICTURE: "assets/img/profile_picture.gif",
 
     DESCRIPTION: '<i>he/him</i><br><br> /\\___/\\<br>꒰ ˶• ༝ - ˶꒱<br>./づ~🍨',
 
-    BADGES: [
-        { id: "HypeSquad Balance", image: "assets/img/hypesquad_balance.png" },
-        { id: "Nitro Sub", image: "assets/img/nitro_sub.svg" },
+    BADGES: [{
+            id: "HypeSquad Balance",
+            image: "assets/img/hypesquad_balance.png"
+        },
+        {
+            id: "Nitro Sub",
+            image: "assets/img/nitro_sub.svg"
+        },
     ],
 
-    PROJECTS: [
-        { id: "amar", image: "assets/img/amar.gif", link: "https://discord.com/servers/amar-444141193022930944", name: "Amar" },
-        { id: "rocketment", image: "assets/img/rocketment.gif", link: "https://discord.com/invite/rocketment", name: "Rocketment" },
-        { id: "ebio", image: "assets/img/ebio.gif", link: "https://discord.com/servers/e-bio-913922450205585439", name: "e.bio" },
-        { id: "Anime & Gaming Cafe", image: "assets/img/animegamingcafe.gif", link: "https://animegamingcafe.de", name: "Anime & Gaming Cafe" },
-        { id: "Reisender Collective", image: "assets/img/reisendercollective.png", link: "https://discord.com/invite/729qxEBEXF", name: "Reisender Collective" },
+    PROJECTS: [{
+            id: "amar",
+            image: "assets/img/amar.gif",
+            link: "https://discord.com/servers/amar-444141193022930944",
+            name: "Amar"
+        },
+        {
+            id: "rocketment",
+            image: "assets/img/rocketment.gif",
+            link: "https://discord.com/invite/rocketment",
+            name: "Rocketment"
+        },
+        {
+            id: "ebio",
+            image: "assets/img/ebio.gif",
+            link: "https://discord.com/servers/e-bio-913922450205585439",
+            name: "e.bio"
+        },
+        {
+            id: "Anime & Gaming Cafe",
+            image: "assets/img/animegamingcafe.gif",
+            link: "https://animegamingcafe.de",
+            name: "Anime & Gaming Cafe"
+        },
+        {
+            id: "Reisender Collective",
+            image: "assets/img/reisendercollective.png",
+            link: "https://discord.com/invite/729qxEBEXF",
+            name: "Reisender Collective"
+        },
     ],
 
-    SOCIALS: [
-        { class_name: "fab fa-github", link: "https://github.com/JustSomeNoname" },
-    ],
+    SOCIALS: [{
+        class_name: "fab fa-github",
+        link: "https://github.com/JustSomeNoname"
+    }, ],
 
 }
