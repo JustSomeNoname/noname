e4b07309b25a26bb4bdd7051fbdfa96685f885a8
JustSomeNoname <noname.discord@protonmail.com>
2023-02-07 15:48:52 +0100

    updated

--- assets/config.js
+++ assets/config.js
@@ -15,46 +15,47 @@ const config = {
         },
     ],*/
 
-    PROJECTS: [{
+    PROJECTS: [
+        {
+            id: "Moderatoren HQ",
+            image: "assets/img/modhq.png",
+            link: "https://discord.com/invite/modhq",
+            name: "Moderatoren HQ",
+            description: "Coming Soon"
+        },
+        {
             id: "amar",
-            image: "assets/img/amar.gif",
+            image: "assets/img/amar.png",
             link: "https://discord.com/servers/amar-444141193022930944",
             name: "Amar",
             description: "Coming Soon"
         },
         {
             id: "rocketment",
-            image: "assets/img/rocketment.gif",
+            image: "assets/img/rocketment.png",
             link: "https://discord.com/invite/rocketment",
             name: "Rocketment",
             description: "Coming Soon"
         },
         {
-            id: "Try discord.com/guidelines",
-            image: "assets/img/guidelines.png",
-            link: "https://discord.com/invite/guidelines",
-            name: "Try discord.com/guidelines",
+            id: "barry's comfy café",
+            image: "assets/img/barryComfyCafe.png",
+            link: "https://discord.com/invite/bawwub",
+            name: "barry's comfy café",
             description: "Coming Soon"
         },
         {
             id: "Anime & Gaming Cafe",
-            image: "assets/img/animegamingcafe.gif",
+            image: "assets/img/animegamingcafe.png",
             link: "https://animegamingcafe.de",
             name: "Anime & Gaming Cafe",
             description: "Coming Soon"
         },
         {
             id: "#TeamRuby",
-            image: "assets/img/rubychan.gif",
+            image: "assets/img/rubychan.png",
             link: "https://discord.com/invite/rubychan",
             name: "#TeamRuby",
-            description: "Coming Soon"
-        },
-        {
-            id: "Reisender Collective",
-            image: "assets/img/reisendercollective.png",
-            link: "https://discord.com/invite/729qxEBEXF",
-            name: "Reisender Collective",
             description: "Coming Soon"
         },
     ],
